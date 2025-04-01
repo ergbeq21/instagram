e30a9eb1c02b4bd222af95787f3f89f618e2c714
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-04-01 23:51:45 +0200

    Added a beautiful style to the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,47 @@
 	<a href="/admin/api/article" class="font-mono transition-all duration-300 ease-in-out hover:text-blue-700 hover:text-xl">Go To Admin Section</a>
 </div>
 
-<main class="grid grid-cols-1 gap-4 p-4 md:grid-cols-3 bg-blue-50">
-	{#each articles as article}
-		<a href="/admin/api/article/{article.id}" class="block bg-white p-2 shadow-md hover:shadow-lg">
-			<img src={article.image} alt={article.description} class="h-48 w-full object-cover" />
-			<p class="mt-2 text-sm">{article.description}</p>
-			<p class="text-xs text-gray-500">By {article.author}</p>
-			<p class="text-xs text-gray-500">Votes: {article.votes}</p>
-		</a>
-	{/each}
-</main>
 
+<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+    <header>
+      <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Image Collection</h2>
 
+      <p class="mt-4 max-w-md text-gray-500">
+		Ein modernes und interaktives Design, das es Nutzern ermöglicht, Artikel 
+		zu liken, Kommentare zu hinterlassen und Antworten zu sehen 
+      </p>
+    </header>
+
+    <ul class="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+		{#each articles as article}
+      <li>
+        <a href="/admin/api/article/{article.id}" class="group block overflow-hidden">
+          <img
+            src={article.image}
+            alt={article.description}
+            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+          />
+
+          <div class="relative bg-white pt-3">
+            <h3 class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+              {article.description}
+            </h3>
+
+            <div class="mt-2">
+              <span class="sr-only"> Votes </span>
+
+			  <div class="flex items-start flex-col">
+				<span class="tracking-wider text-gray-900 text-xs">Author: {article.author} </span>
+				<span class="tracking-wider text-gray-900 text-xs">Votes: {article.votes} </span>
+
+			  </div>
+              
+            </div>
+          </div>
+        </a>
+      </li>
+	  {/each}
+    </ul>
+  </div>
+</section>
