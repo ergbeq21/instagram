c6cf8433bee57b78257c555041c32d2259f790da
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-04-02 09:18:31 +0200

    Change the Port

--- src/routes/admin/comments/+page.server.js
+++ src/routes/admin/comments/+page.server.js
@@ -2,8 +2,6 @@ import { createConnection } from '$lib/db/mysql';
 
 import { redirect } from '@sveltejs/kit';
 
-import { redirect } from '@sveltejs/kit';
-
 export async function load({ locals }) {
 	if (!locals.user || locals.user.role !== 'admin') {
 		redirect(302, '/login');
