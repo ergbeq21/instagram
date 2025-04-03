1600bc4cfac7dd4d6d65eb66c9597170a08ada28
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-04-04 00:23:37 +0200

    Making functionality to chat section4

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -143,7 +143,7 @@
                 <summary>See your messages with {form.userInfo.username}</summary>
                 {#each data.messages as message}
                     {#if (message.user_id === form.userInfo.id && message.user2_id === data.user.id) || (message.user_id === data.user.id && message.user2_id === form.userInfo.id)}
-                        <p>{message.user_id === data.user.id ? 'You' : form.userInfo.username}: {message.text}</p>
+                        <p>{message.user_id === form.userInfo.id ? 'You' : form.userInfo.username}: {message.text}</p>
                     {/if}
                 {/each}
             </details>
@@ -153,7 +153,7 @@
     <div class="ml-10">
         {#if form && form.userInfo}
             <form action="?/sendMessage" method="POST" class="bg-white shadow-lg rounded-lg p-6 w-80" use:enhance>
-                <p class="font-semibold text-sm mb-2">Send Message to {form.userInfo.username} (ID: {form.userInfo.id})</p>
+                <p class="font-semibold text-sm mb-2">Send Message to {form.userInfo.username}</p>
                 <input type="hidden" name="userID" value={form.userInfo.id}>
                 <input type="hidden" name="user2ID" value={data.user.id}>
                 <input type="text" name="text" class="w-full h-10 px-4 border rounded-lg mb-3 text-sm" placeholder="Write a message...">
